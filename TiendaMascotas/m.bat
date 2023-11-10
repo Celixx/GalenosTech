17e2675fef3bc554cd606ef033e5b76f0b738a09
programat3 <li.dasilva@duocuc.cl>
2023-11-10 20:47:57 -0300

    cambiosLia

--- TiendaMascotas/m.bat
+++ TiendaMascotas/m.bat
@@ -1,4 +1,4 @@
-call C:\ProyectosDjango\Tienda-Mascotas-2\TiendaMascotas_venv\Scripts\activate.bat
+call C:\Users\liada\OneDrive\Documentos\GalenosTech\TiendaMascotas\TiendaMascotas_venv\Scripts\activate.bat
 call python manage.py makemigrations
 call python manage.py makemigrations core
 call python manage.py migrate
